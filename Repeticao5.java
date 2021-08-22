31a4802cbd54e6776abb49b68b1071302bc24506
xKaue007 <kaueoliveira92@gmail.com>
2021-08-22 20:28:18 +0000

    Ex.5

---
+++ Repeticao5.java
@@ -0,0 +1,12 @@
+public class Repeticao5{
+    public static void main(String args[]){
+
+        int valor=10;
+        if(valor%2==0){
+            System.out.println("PAR");
+        }
+        else{
+            System.out.println("IMPAR");
+        }
+    }
+}
